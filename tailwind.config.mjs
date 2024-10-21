3427c173c438fd8c9233cb5adcce63bd973c4378
Natalia <alamo.natalia@gmail.com>
2024-10-21 13:53:22 +0200

    add css custom propierties with tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	// darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				ftnBrandMain:{
+					100: "var(--brandMain100)",
+					900: "var(--brandMain900)"
+				},
+			},
+		},
+		
 	},
 	plugins: [],
 }
