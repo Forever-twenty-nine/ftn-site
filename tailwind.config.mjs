dfb40a687a9fe599db2241ff3807aea4be241126
Natalia <alamo.natalia@gmail.com>
2024-11-03 16:17:01 +0100

    added pallete color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,64 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	// darkMode: "class",
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+		},
 		extend: {
 			colors: {
-				ftnBrandMain:{
-					100: "var(--brandMain100)",
-					900: "var(--brandMain900)"
+				brand_primary:{
+					50: "var(--brand_primary_50)",
+					100: "var(--brand_primary_100)",
+					200: "var(--brand_primary_200)",
+					300: "var(--brand_primary_300)",
+					400: "var(--brand_primary_400)",
+					500: "var(--brand_primary_500)",
+					600: "var(--brand_primary_600)",
+					700: "var(--brand_primary_700)",
+					800: "var(--brand_primary_800)",
+					900: "var(--brand_primary_900)",
+					950: "var(--brand_primary_950)",
 				},
+				brand_secondary:{
+					50: "var(--brand_neutral_50)",
+					100: "var(--brand_neutral_100)",
+					200: "var(--brand_neutral_200)",
+					300: "var(--brand_neutral_300)",
+					400: "var(--brand_neutral_400)",
+					500: "var(--brand_neutral_500)",
+					600: "var(--brand_neutral_600)",
+					700: "var(--brand_neutral_700)",
+					800: "var(--brand_neutral_800)",
+					900: "var(--brand_neutral_900)",
+					950: "var(--brand_neutral_950)",
+				},
+				brand_tertiary:{
+					50: "var(--brand_neutral_50)",
+					100: "var(--brand_neutral_100)",
+					200: "var(--brand_neutral_200)",
+					300: "var(--brand_neutral_300)",
+					400: "var(--brand_neutral_400)",
+					500: "var(--brand_neutral_500)",
+					600: "var(--brand_neutral_600)",
+					700: "var(--brand_neutral_700)",
+					800: "var(--brand_neutral_800)",
+					900: "var(--brand_neutral_900)",
+					950: "var(--brand_neutral_950)",
+				},
+				brand_neutral:{
+					50: "var(--brand_neutral_50)",
+					100: "var(--brand_neutral_100)",
+					200: "var(--brand_neutral_200)",
+					300: "var(--brand_neutral_300)",
+					400: "var(--brand_neutral_400)",
+					500: "var(--brand_neutral_500)",
+					600: "var(--brand_neutral_600)",
+					700: "var(--brand_neutral_700)",
+					800: "var(--brand_neutral_800)",
+					900: "var(--brand_neutral_900)",
+					950: "var(--brand_neutral_950)",
+				}
 			},
 		},
 		
